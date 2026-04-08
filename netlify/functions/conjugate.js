2cdae6173ef3a76021103d94bf137d1377cb9f0d
tedfreedman <tedfreedman489@gmail.com>
2026-04-07 22:40:40 -0400

    Update conjugate.js

--- netlify/functions/conjugate.js
+++ netlify/functions/conjugate.js
@@ -36,7 +36,7 @@ k must be one of: present, passe, imparfait, futur, cond, condp, plusque, subj, 
 
   const requestBody = JSON.stringify({
     model: 'claude-haiku-4-5-20251001',
-    max_tokens: 3000,
+    max_tokens: 6000,
     messages: [{ role: 'user', content: prompt }]
   });
 
